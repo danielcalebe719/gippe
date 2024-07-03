3f59c57d78f25eb691d2bee416bc7f4eaa9f678f
Arjuna <arjunapinto@gmail.com>
2024-07-03 13:39:36 -0300

    Servicos

--- laravel/resources/views/website/servicos.blade.php
+++ laravel/resources/views/website/servicos.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Buffet Divino Sabor - Serviços</title>
+    <title>aBuffet Divino Sabor - Serviços</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
     <script src="jsservices.js"></script>
