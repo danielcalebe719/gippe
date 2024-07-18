f773163d3d81ed1cefbfa8b458ac4a55bfb4432b
Fabricio7473 <0001020119@senaimgaluno.com.br>
2024-07-18 14:03:27 -0300

    arrumei menu agr tem que verificar que acessa cada pagina do adm

--- laravel/resources/views/adm/templates/template.blade.php
+++ laravel/resources/views/adm/templates/template.blade.php
@@ -135,7 +135,7 @@
                 </li>
                 <li class="nav-item active">
                 <a href="{{url('adm/mensagens')}}" class="nav-link">
-                <img class='fas fa-fw fa-tachometer-alt' src="{{ asset('assets/img/clientes.png') }}" alt="">
+                <img class='fas fa-fw fa-tachometer-alt' src="{{ asset('assets/img/notificacoes.png') }}" alt="">
                         <span class="text-gray-1000">Mensagens</span>
                 </a>
                 </li>
@@ -145,13 +145,6 @@
                         <span class="text-gray-1000">Colaboradores</span>
                     </a>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{url('adm/mensagens')}}">
-                        <img class='fas fa-fw fa-tachometer-alt' src="{{ asset('assets/img/notificacoes.png') }}" alt="">
-                        <span class="text-gray-1000">Mensagens</span>
-                    </a>
-                </li>
-            
             </div>
             @endif
             <hr class="sidebar-divider my-0">
@@ -162,12 +155,6 @@
                     <a class="nav-link" href="{{url('adm/painel-financeiro')}}">
                         <img class="fas fa-fw fa-tachometer-alt" src="{{ asset('assets/img/menu-img1.png') }}" alt="">
                         <span class="text-gray-1000">Painel Financeiro</span>
-                    </a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{url('adm/painel-financeiro')}}s">
-                        <img class="fas fa-fw fa-tachometer-alt" src="{{ asset('assets/img/menu-img2.png') }}" alt="">
-                        <span class="text-gray-1000">Relatórios Gráficos</span>
                     </a>
                 </li>
                 <li class="nav-item active">
